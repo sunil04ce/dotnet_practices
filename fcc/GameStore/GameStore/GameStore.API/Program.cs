5203f075091c19624e2d819e548e83ba3d612881
sunil parmar <sunil04ce@gmail.com>
2024-08-03 22:18:36 +0530

    added get end points using GameDto list

--- fcc/GameStore/GameStore/GameStore.API/Program.cs
+++ fcc/GameStore/GameStore/GameStore.API/Program.cs
@@ -1,7 +1,36 @@
+using GameStore.API.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new (
+        1,
+        "Street Fighter II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992, 7, 15)),
+    new (
+        2,
+        "Final Fantasy XIV",
+        "Roleplaying",
+        59.99M,
+        new DateOnly(2010, 8, 30)),        
+    new (
+        3,
+        "Fifa 2023",
+        "Sports",
+        69.99M,
+        new DateOnly(2022, 9, 27))
+];
+
+// app.MapGet("/", () => "Hello World!");
+
+// GET /games
+app.MapGet("games", () => games);
+
+// GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
 
 app.Run();
