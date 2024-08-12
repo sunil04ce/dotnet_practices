0ca102d1fc16ecbf55d9e61a61b401420f5b1236
sunil parmar <sunil04ce@gmail.com>
2024-08-12 21:57:17 +0530

    added entities

---
+++ fcc/GameStore/GameStore/GameStore.API/Entities/Game.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GameStore.API.Entities;
+
+public class Game
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+
+    public int GenreId { get; set; }
+    public Genre? Genre { get; set; }
+}
