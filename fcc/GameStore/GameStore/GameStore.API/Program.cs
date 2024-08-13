c548763fd05d68a468c49e42971fdc747243011a
sunil parmar <sunil04ce@gmail.com>
2024-08-13 23:13:59 +0530

    added sqlite nuget, connString

--- fcc/GameStore/GameStore/GameStore.API/Program.cs
+++ fcc/GameStore/GameStore/GameStore.API/Program.cs
@@ -1,6 +1,10 @@
+using GameStore.API.Data;
 using GameStore.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
