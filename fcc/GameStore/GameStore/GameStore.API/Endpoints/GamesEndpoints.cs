01cd8a338c8f606ff00e55b45dabaebe800f12dc
sunil parmar <sunil04ce@gmail.com>
2024-08-16 21:55:30 +0530

    stored POST data to db, ef second migration

--- fcc/GameStore/GameStore/GameStore.API/Endpoints/GamesEndpoints.cs
+++ fcc/GameStore/GameStore/GameStore.API/Endpoints/GamesEndpoints.cs
@@ -1,5 +1,7 @@
 using System;
+using GameStore.API.Data;
 using GameStore.API.Dtos;
+using GameStore.API.Entities;
 
 namespace GameStore.API.Endpoints;
 
@@ -46,16 +48,28 @@ public static class GamesEndpoints
         .WithName(GetGameEndpointName);
 
         // POST /games
-        group.MapPost("/", (CreateGameDto newGame) =>
+        group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) =>
         {
-            GameDto game = new(
-                games.Count + 1,
-                newGame.Name,
-                newGame.Genre,
-                newGame.Price,
-                newGame.ReleaseDate);
-            games.Add(game);
-            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+            Game game = new()
+            {
+                Name = newGame.Name,
+                Genre = dbContext.Genres.Find(newGame.GenreId),
+                GenreId = newGame.GenreId,
+                Price = newGame.Price,
+                ReleaseDate = newGame.ReleaseDate
+            };
+            dbContext.Games.Add(game);
+            dbContext.SaveChanges();
+
+            GameDto gameDto = new(
+                game.Id,
+                game.Name,
+                game.Genre!.Name,
+                game.Price,
+                game.ReleaseDate
+            );
+
+            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, gameDto);
         });
 
         // PUT /games/1
