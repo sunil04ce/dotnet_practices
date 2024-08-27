4a3bd15b90b46d753f2515b621589542294ae0f9
sunil parmar <sunil04ce@gmail.com>
2024-08-27 22:03:26 +0530

    action method for Edit category

--- netmastery/Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ netmastery/Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -42,5 +42,44 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id ==0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+            //Category? category2 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category? category3 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            //if (obj.Name != null && obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            //}
+            //if (obj.Name != null && obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("", "Test is an invalid value");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
