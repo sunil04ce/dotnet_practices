400363ca99545ada14c87910f3dbd10620aa9f26
sunil parmar <sunil04ce@gmail.com>
2024-09-02 22:16:41 +0530

    added edit, delete feature

--- netmastery/Bulky/BulkyWebRazor/Pages/Categories/Create.cshtml.cs
+++ netmastery/Bulky/BulkyWebRazor/Pages/Categories/Create.cshtml.cs
@@ -1,12 +1,15 @@
 using BulkyWebRazor.Data;
 using BulkyWebRazor.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkyWebRazor.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+        //[BindProperty]
         public Category Category { get; set; }
         public CreateModel(ApplicationDbContext db)
         {
@@ -14,6 +17,13 @@ namespace BulkyWebRazor.Pages.Categories
         }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
