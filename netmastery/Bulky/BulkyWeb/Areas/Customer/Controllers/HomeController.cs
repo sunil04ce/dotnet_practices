0ad22d596e123a2f804dd34d5ea6dd2661b7cce5
sunil parmar <sunil04ce@gmail.com>
2024-09-20 22:53:22 +0530

    displayed products with price in home page

--- netmastery/Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ netmastery/Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +9,18 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork , ILogger<HomeController> logger)
         {
+            _unitOfWork = unitOfWork;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
