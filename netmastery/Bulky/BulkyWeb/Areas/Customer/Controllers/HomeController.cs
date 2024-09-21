d42dc8d89629a863d8db333a2d21adbdb45fa1b2
sunil parmar <sunil04ce@gmail.com>
2024-09-21 10:52:31 +0530

    displayed product details

--- netmastery/Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ netmastery/Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(IUnitOfWork unitOfWork , ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork, ILogger<HomeController> logger)
         {
             _unitOfWork = unitOfWork;
             _logger = logger;
@@ -21,6 +21,12 @@ namespace BulkyWeb.Areas.Customer.Controllers
         {
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
             return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
