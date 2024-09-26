cf2072d35564521d501ca21c808147de1b51d8b9
sunil parmar <sunil04ce@gmail.com>
2024-09-26 23:15:35 +0530

    added ApplicationUser, migration of IdentityTables,User

--- netmastery/Bulky/BulkyWeb/Program.cs
+++ netmastery/Bulky/BulkyWeb/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
@@ -31,6 +32,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
