99d63eb523b82f1ec92699d13031d2a28a1f1d14
sunil parmar <sunil04ce@gmail.com>
2024-10-01 22:20:34 +0530

    added Role based access

--- netmastery/Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ netmastery/Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using Bulky.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
